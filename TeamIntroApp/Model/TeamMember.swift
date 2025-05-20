566662471a1cfbd67fabfb4c9ed387961cdeebd3
김성연 <37677444+yeon1216@users.noreply.github.com>
2025-05-20 17:39:01 +0900

    feat: 팀원 블로그 웹뷰로 보는 기능 추가 (#2)

---
+++ TeamIntroApp/Model/TeamMember.swift
@@ -0,0 +1,17 @@
+//
+//  TeamMember.swift
+//  TeamIntroApp
+//
+//  Created by ksy on 5/20/25.
+//
+
+import Foundation
+
+struct TeamMember {
+    let name: String
+    let mbti: String
+    let description: String
+    let strengths: [String]
+    let collaborationStyle: String
+    let blogURL: URL
+} 
