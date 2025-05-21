892edf165c92dc546b34d30eaf43743b99c29e45
SeorinLee <yms9074@gmail.com>
2025-05-21 17:31:18 +0900

    디자인 수정

--- TeamIntroApp/MemberDetailViewController.swift
+++ TeamIntroApp/MemberDetailViewController.swift
@@ -7,6 +7,10 @@ class MemberDetailViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
+        
+        if let member = member {
+            navigationItem.title = member.name
+        }
 
         setupUI()
     }
@@ -17,13 +21,13 @@ class MemberDetailViewController: UIViewController {
         var yOffset: CGFloat = 100
         
         let imageView = UIImageView()
-        imageView.frame = CGRect(x: (view.frame.width - 100) / 2, y: yOffset, width: 100, height: 100)
+        imageView.frame = CGRect(x: (view.frame.width - 200) / 2, y: yOffset, width: 200, height: 200) 
         imageView.image = UIImage(named: member.imageName)
-        imageView.contentMode = .scaleAspectFill
+        imageView.contentMode = .scaleAspectFit   // ⬅️ 사각형 이미지 왜곡 없이 표시
         imageView.clipsToBounds = true
-        imageView.layer.cornerRadius = 50
+        // imageView.layer.cornerRadius = 50 // ⛔️ 사각형을 원하지 않으면 이 줄 제거!
         view.addSubview(imageView)
-        yOffset += 120
+        yOffset += 220 // 크기만큼 Y 오프셋 조정
 
         let nameLabel = makeLabel(text: "이름: \(member.name)", y: yOffset)
         yOffset += 30
