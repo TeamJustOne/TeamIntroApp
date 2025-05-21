5ff5c38be832a15f1dfd3510f928cae89dcf8a1f
Wooseong <thehousedone@gmail.com>
2025-05-21 09:38:58 +0900

    ImageView 이미지 에셋에 추가, TeamInfoViewController 생성

---
+++ TeamIntroApp/TeamInfoViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TeamInfoViewController.swift
+//  TeamIntroApp
+//
+//  Created by 김우성 on 5/21/25.
+//
+
+import UIKit
+
+class TeamInfoViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
