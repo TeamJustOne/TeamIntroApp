3cebb52b994df76526d6ee44c618b23a1605bb6c
Pjh01 <estelle0102@naver.com>
2025-05-21 12:00:47 +0900

    Chore: SplashView 수정

--- TeamIntroApp/SplashViewController.swift
+++ TeamIntroApp/SplashViewController.swift
@@ -12,32 +12,56 @@ class SplashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        view.backgroundColor = .systemCyan
+        setupUI()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0){
+            self.showMainView()
+        }
+    }
+    
+    private func setupUI() {
+        view.backgroundColor = .white
+        
+        let circleLabel = UILabel()
+        circleLabel.text = "TJO"
+        circleLabel.textAlignment = .center
+        circleLabel.backgroundColor = .darkGray
+        circleLabel.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+        circleLabel.textColor = .white
+        circleLabel.layer.cornerRadius = 25
+        circleLabel.layer.borderWidth = 2
+        circleLabel.layer.borderColor = UIColor.darkGray.cgColor
+        circleLabel.layer.masksToBounds = true
+        circleLabel.translatesAutoresizingMaskIntoConstraints = false
+        view.addSubview(circleLabel)
         
         let titleLabel = UILabel()
         titleLabel.text = "Team Just One"
         titleLabel.textColor = .white
-        titleLabel.font = UIFont.boldSystemFont(ofSize: 32)
+        titleLabel.font = UIFont.systemFont(ofSize: 36, weight: .bold)
+        titleLabel.textColor = .darkGray
+        titleLabel.textAlignment = .center
         titleLabel.translatesAutoresizingMaskIntoConstraints = false
         view.addSubview(titleLabel)
         
         NSLayoutConstraint.activate([
             titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
-            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            
+            circleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            circleLabel.bottomAnchor.constraint(equalTo: titleLabel.topAnchor, constant: -12),
+            circleLabel.widthAnchor.constraint(equalToConstant: 50),
+            circleLabel.heightAnchor.constraint(equalToConstant: 50)
         ])
-        
-        DispatchQueue.main.asyncAfter(deadline: .now() + 3.0){
-            self.showMainView()
-        }
     }
     
     private func showMainView() {
         let storyboard = UIStoryboard(name: "Main", bundle: nil)
         let mainVC = storyboard.instantiateViewController(withIdentifier: "MainViewController")
-        
+        let navVC = UINavigationController(rootViewController: mainVC)
         
         if let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene, let window = windowScene.windows.first {
-                window.rootViewController = mainVC
+                window.rootViewController = navVC
                 window.makeKeyAndVisible()
         }
     }
